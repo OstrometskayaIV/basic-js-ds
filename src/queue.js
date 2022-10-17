125199afd6d2adc001da91da8e1a714d0b4ac4de
Irina Ostrometskaya <i.ostrometskaya@gmail.com>
2022-10-18 00:01:58 +0300

    feat: find solution for queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +13,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class Queue {
+  
+  constructor() {
+    this.firstElement = null;
+    this.lastElement = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.firstElement.next, this.firstElement;
+  }
+
+  enqueue(value) {
+    const node = new Node(value);
+
+    if (!this.firstElement) {
+      this.firstElement = node;
+      this.lastElement = node;
+    } else {
+      this.lastElement.next = node;
+      this.lastElement = node;
+    }
+    return this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.firstElement) {
+      return null;
+    }
+
+    let dequeueElement = this.firstElement;
+
+    if (this.firstElement === this.lastElement) {
+      this.lastElement = null;
+    }else {
+    this.firstElement = this.firstElement.next;
+    this.length--;
+    return dequeueElement.value;
+    }
   }
 }
 
