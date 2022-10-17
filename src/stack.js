edaec827ddafeae3235a80a9d73d491a06c69d84
Irina Ostrometskaya <i.ostrometskaya@gmail.com>
2022-10-17 22:53:52 +0300

    feat: find solution for stack

--- src/stack.js
+++ src/stack.js
@@ -4,7 +4,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement the Stack with a given interface via array.
  *
  * @example
- * const stack = new Stack();
+ * 
  *
  * stack.push(1); // adds the element to the stack
  * stack.peek(); // returns the peek, but doesn't delete it, returns 1
@@ -12,20 +12,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
+
+
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor() {
+    this.stack = [];
+  }
+
+  push(element) {
+    this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.stack.length == 0) {
+      return undefined;
+    } 
+    return this.stack.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length - 1];
   }
 }
 
